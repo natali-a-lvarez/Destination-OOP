6224bc4253de6fe0a5be677eda42cc45a666ee38
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2024-02-08 12:11:14 -0600

    added Person class and Person test

---
+++ __tests__/classes/Person.test.js
@@ -0,0 +1,36 @@
+const Person = require('../../classes/Person')
+const Bag = require('../../classes/Bag')
+
+//testing that we can create instance of Person
+describe('Person test', () => {
+    test('should create an instance of a Person', () => {
+        const person1 = new Person('ibrahim', 'New York City');
+        expect(person1).toBeInstanceOf(Person)
+    })
+//testing name and destination
+test('should set name and destination correctly', () => {
+    const name = 'ibrahim';
+    const destination = 'New York City';
+    const person2 = new Person(name, destination);
+    expect(person2.name).toBe(name);
+    expect(person2.destination).toBe(destination);
+})
+
+//testing that bags is an array
+test('should return an empty array of bags', () => {
+    const name = "ibrahim"
+    const destination = 'Chicago'
+    const person3 = new Person(name, destination)
+    expect(person3.bags).toMatchObject([])
+})
+
+// //testing that addBags adds an item to the array
+// test('should return an array length of 1', () => {
+//     const name = "ibrahim"
+//     const destination = 'Chicago'
+//     const bag = new Bag()
+//     const person3 = new Person(name, destination, bag)
+//     expect(person3.bags).toMatchObject([])
+// })
+
+})
